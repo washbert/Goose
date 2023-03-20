a3aabac3d615f6d18a300822753b12a15bf7d996
Bryan Haye <bhaye@realdecoy.com>
2023-03-20 16:00:29 -0500

    Policy Updates

--- .infra/prod/main.tf
+++ .infra/prod/main.tf
@@ -21,10 +21,25 @@ provider "aws" {
 }
 }
 
-module "main_site" {
-  source = "/"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  vpc_tags = {
+    Name = "goose-vpc"
+  }
+  name = "goose-vpc"
+  cidr = "172.0.0.0/16"
 
-  base_dir = "${path.module}/dist"
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["172.0.1.0/24", "172.0.2.0/24", "172.0.3.0/24"]
+  public_subnets  = ["172.0.101.0/24", "172.0.102.0/24", "172.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "prod"
+  }
 }
 
 resource "aws_s3_bucket" "goose_static_site" {
@@ -40,17 +55,43 @@ resource "aws_s3_bucket_public_access_block" "static_site_policy" {
   restrict_public_buckets = false
 }
 
+data "aws_canonical_user_id" "current" {}
+
 resource "aws_s3_bucket_acl" "goose_static_acl" {
   bucket = aws_s3_bucket.goose_static_site.id
-  acl = "public-read"
+  access_control_policy {
+    grant {
+      grantee {
+        type = "Group"
+        uri  = "http://acs.amazonaws.com/groups/global/AllUsers"
+      }
+      permission = "READ_ACP"
+    }
+    owner {
+      id = data.aws_canonical_user_id.current.id
+    }
+  }
 
 }
 
 resource "aws_s3_bucket_policy" "static_site_policy" {
   bucket = aws_s3_bucket.goose_static_site.id
 
-  policy = (public-access-policy.json)
+  policy = data.aws_iam_policy_document.allow_complete_access_to_everyone.json
 
+}
+
+data "aws_iam_policy_document" "allow_complete_access_to_everyone"{
+  statement {
+    sid = "1"
+
+    actions = [
+      "s3:*",
+    ]
+    resources = [
+      "arn:aws:s3:::goose-social-terraform/*",
+    ]
+  }
 }
 
 resource "aws_s3_bucket_website_configuration" "static_site_configuration" {
@@ -59,19 +100,4 @@ resource "aws_s3_bucket_website_configuration" "static_site_configuration" {
   index_document {
     suffix = "index.html"
   }
-}
-
-resource "aws_s3_object" "static_site_files" {
-  bucket = aws_s3_bucket.goose_static_site.id
-
-  for_each = module.main_site.files
-
-  key = each.key
-
-  content_type = each.value.content_type
-
-  source = each.value.source_path
-  content = each.value.content
-
-  etag = each.value.digests.md5
 }
